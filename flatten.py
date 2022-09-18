3469510c787eb0ed1bdfb877ff817cabb72e57da
ngoc <ptbichngoc2502@gmail.com>
2022-09-19 05:21:45 +0700

    defined fatten layer for baches and multi chanels

--- flatten.py
+++ flatten.py
@@ -1,14 +1,33 @@
 import numpy as np
 
-def Flatten(A:np.array):
+def Flatten_step(A):
     A=A.flatten()
     return  A
 
-def Flatten_backprop(A,prev_size):
+def Flatten_backprop_step(A,prev_size):
     A=A.reshape(prev_size)
-    return A
+    return A     
 
-# A= np.array([[1,2,3,4],[0,0,4,5],[2,2,2,5],[0,9,8,1]])
-# print(Flatten(A))
-# size=4,4
-# print(Flatten_backprop(A,size))
+def Flatt_forward(A):
+    (batch, height, width, n_chanel)=A.shape
+    a_flat=np.zeros((batch,height*width*n_chanel))
+    for i in range(batch):
+        a_pre=A[i]
+        a_flat[i,:]=Flatten_step(a_pre)
+    
+    cache_size= height, width, n_chanel
+    return a_flat,cache_size
+       
+def Flatt_backward(A,pre_size):
+    (batch,size)=A.shape
+    height,width,n_chanel=pre_size
+    back_prop_A=np.zeros((batch,height,width,n_chanel))
+    for i in range (batch):
+        back_prop_A[i,:,:,:]=Flatten_backprop_step(A[i,:],pre_size)
+    return back_prop_A
+
+np.random.seed(1)
+A_pre = np.random.randn(5,5,3,2)
+a_flat,cache_size= Flatt_forward(A_pre)
+a_back=Flatt_backward(a_flat,cache_size)
+print(A_pre==a_back)
